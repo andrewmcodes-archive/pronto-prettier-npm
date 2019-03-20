2275e66443c04b6596b20ee6821c43bfdf1d37bc
Andrew Mason <andrewmcodes@protonmail.com>
2019-03-19 22:10:30 -0400

    start process of creating new way to test the gem

--- spec/pronto/prettier/npm_spec.rb
+++ spec/pronto/prettier/npm_spec.rb
@@ -24,71 +24,72 @@ RSpec.describe Pronto::Prettier::Npm do
         expect(run).to eql([])
       end
     end
-
-    context "patches with a one and a four warnings" do
-      include_context "test repo"
-
-      let(:patches) { repo.diff("master") }
-
-      it "returns correct number of errors" do
-        expect(run.count).to eql(5)
-      end
-
-      it "has correct first message" do
-        expect(run.first.msg).to eql("'foo' is not defined.")
-      end
-
-      context(
-        "with files to lint config that never matches",
-        config: { "files_to_lint" => "will never match" },
-      ) do
-        it "returns zero errors" do
-          expect(run.count).to eql(0)
-        end
-      end
-
-      context(
-        "with files to lint config that matches only .js",
-        config: { "files_to_lint" => '\.js$' },
-      ) do
-        it "returns correct amount of errors" do
-          expect(run.count).to eql(2)
-        end
-      end
-
-      context(
-        "with cmd_line_opts to include .html",
-        config: { "cmd_line_opts" => "--ext .html" },
-      ) do
-        it "returns correct number of errors" do
-          expect(run.count).to eql 5
-        end
-      end
-
-      context(
-        "with different prettier executable",
-        config: { "prettier_executable" => "./custom_prettier.sh" },
-      ) do
-        it "calls the custom prettier prettier_executable" do
-          expect { run }.to raise_error(JSON::ParserError, /custom prettier called/)
-        end
-      end
-    end
-
-    context "repo with ignored and not ignored file, each with three warnings" do
-      include_context "prettierignore repo"
-
-      let(:patches) { repo.diff("master") }
-
-      it "returns correct number of errors" do
-        expect(run.count).to eql(3)
-      end
-
-      it "has correct first message" do
-        expect(run.first.msg).to eql("'HelloWorld' is defined but never used.")
-      end
-    end
   end
+
+  #   context "patches with a one and a four warnings" do
+  #     # include_context "test repo"
+
+  #     let(:patches) { repo.diff("master") }
+
+  #     it "returns correct number of errors" do
+  #       expect(run.count).to eql(5)
+  #     end
+
+  #     it "has correct first message" do
+  #       expect(run.first.msg).to eql("'foo' is not defined.")
+  #     end
+
+  #     context(
+  #       "with files to lint config that never matches",
+  #       config: { "files_to_lint" => "will never match" },
+  #     ) do
+  #       it "returns zero errors" do
+  #         expect(run.count).to eql(0)
+  #       end
+  #     end
+
+  #     context(
+  #       "with files to lint config that matches only .js",
+  #       config: { "files_to_lint" => '\.js$' },
+  #     ) do
+  #       it "returns correct amount of errors" do
+  #         expect(run.count).to eql(2)
+  #       end
+  #     end
+
+  #     context(
+  #       "with cmd_line_opts to include .html",
+  #       config: { "cmd_line_opts" => "--ext .html" },
+  #     ) do
+  #       it "returns correct number of errors" do
+  #         expect(run.count).to eql 5
+  #       end
+  #     end
+
+  #     context(
+  #       "with different prettier executable",
+  #       config: { "prettier_executable" => "./custom_prettier.sh" },
+  #     ) do
+  #       it "calls the custom prettier prettier_executable" do
+  #         expect { run }.to raise_error(JSON::ParserError, /custom prettier called/)
+  #       end
+  #     end
+  #   end
+
+  #   context "repo with ignored and not ignored file, each with three warnings" do
+  #     include_context "prettierignore repo"
+
+  #     let(:patches) { repo.diff("master") }
+
+  #     it "returns correct number of errors" do
+  #       expect(run.count).to eql(3)
+  #     end
+
+  #     it "has correct first message" do
+  #       expect(run.first.msg).to eql("'HelloWorld' is defined but never used.")
+  #     end
+  #   end
+  # end
 
   describe "#files_to_lint" do
     subject(:files_to_lint) { prettier.files_to_lint }
@@ -113,15 +114,15 @@ RSpec.describe Pronto::Prettier::Npm do
       expect(prettier_executable).to eql("prettier")
     end
 
-    context(
-      "with different prettier executable config",
-      config: { "prettier_executable" => "custom_prettier" },
-    ) do
-      it "is correct" do
-        prettier.read_config
-        expect(prettier_executable).to eql("custom_prettier")
-      end
-    end
+    # context(
+    #   "with different prettier executable config",
+    #   config: { "prettier_executable" => "custom_prettier" },
+    # ) do
+    #   it "is correct" do
+    #     prettier.read_config
+    #     expect(prettier_executable).to eql("custom_prettier")
+    #   end
+    # end
   end
 
   describe "#prettier_command_line" do
@@ -152,14 +153,14 @@ RSpec.describe Pronto::Prettier::Npm do
       end
     end
 
-    context(
-      "with some command line options",
-      config: { "cmd_line_opts" => "--my command --line opts" },
-    ) do
-      it "includes the custom command line options" do
-        prettier.read_config
-        expect(prettier_command_line).to include("--my command --line opts")
-      end
-    end
+    # context(
+    #   "with some command line options",
+    #   config: { "cmd_line_opts" => "--my command --line opts" },
+    # ) do
+    #   it "includes the custom command line options" do
+    #     prettier.read_config
+    #     expect(prettier_command_line).to include("--my command --line opts")
+    #   end
+    # end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,29 +5,8 @@ require "bundler/setup"
 require "pronto/prettier/npm"
 require "simplecov"
 require "pry"
+
 SimpleCov.start
-
-%w(test prettierignore).each do |repo_name|
-  RSpec.shared_context "#{repo_name} repo" do
-    let(:git) { "spec/fixtures/#{repo_name}.git/git" }
-    let(:dot_git) { "spec/fixtures/#{repo_name}.git/.git" }
-
-    before { FileUtils.mv(git, dot_git) }
-    let(:repo) { Pronto::Git::Repository.new("spec/fixtures/#{repo_name}.git") }
-    after { FileUtils.mv(dot_git, git) }
-  end
-end
-
-RSpec.shared_context "with config", config: true do
-  requested_config = metadata[:config]
-
-  before(:each) do
-    allow_any_instance_of(Check).to receive(:config_options).and_return(requested_config)
-
-    # make sure the config is actually read in the example
-    expect_any_instance_of(Check).to receive(:read_config).and_call_original
-  end
-end
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
